7170b4dbe1f5a7366fd565d99fa0987d2b0bc31a
Kate Efimova <kate.efimova0@gmail.com>
2019-08-09 11:50:27 -0700

    :fire:

--- Week 5/github-usercard/GitHubCard/index.js
+++ Week 5/github-usercard/GitHubCard/index.js
@@ -3,6 +3,16 @@
            https://api.github.com/users/<your name>
 */
 
+axios
+  .get("https://api.github.com/users/kefimochi")
+  .then(response => {
+    console.log(response.data);
+    // deal with the response data in here
+  })
+  .catch(err => {
+    // deal with the error in here
+  });
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -53,3 +63,89 @@ const followersArray = [];
   luishrd
   bigknell
 */
+// Goes to a provided url and converts the received data into a JSON object
+axios
+  .get("https://api.github.com/search/users?q=roman", {
+    method: "get"
+  })
+  .then(res => {
+    res.data.items.length !== 0 ? addUsersToDOM(res.data) : false;
+    // res.json().then(result => {
+    //   console.log("result", result);
+    //   result.total_count !== 0 ? addUsersToDOM(result) : noUsersFound();
+    // });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+function addUsersToDOM(users) {
+  console.log("USERTEMS", users.items);
+  users.items.forEach(person => {
+    // Prevents undefined profiles from showing
+    if (person === undefined) return;
+    // Shows small cards for all found users
+    extractUserObject(person);
+  });
+}
+
+function extractUserObject(user) {
+  axios
+    .get(user.url, {
+      method: "get"
+    })
+    .then(res => {
+      console.log("Res", res.data);
+      createUserComponent(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+let cards = document.querySelector(".cards");
+
+function createUserComponent(user) {
+  console.log(user);
+  let simpleUser = document.createElement("div");
+  simpleUser.classList.add("simple-user");
+  cards.appendChild(simpleUser);
+
+  let topDescription = document.createElement("div");
+  topDescription.classList.add("top-description");
+  simpleUser.appendChild(topDescription);
+
+  let avatar = document.createElement("img");
+  avatar.setAttribute("src", user.avatar_url);
+  topDescription.appendChild(avatar);
+
+  let userShortInfo = document.createElement("div");
+  userShortInfo.classList.add("user-short-info");
+  topDescription.appendChild(userShortInfo);
+
+  let name = document.createElement("h3");
+  name.textContent = user.name;
+  userShortInfo.appendChild(name);
+
+  let login = document.createElement("span");
+  login.textContent = `@${user.login}`;
+  name.appendChild(login);
+
+  let desciption = document.createElement("p");
+  desciption.textContent = user.bio;
+  userShortInfo.appendChild(desciption);
+
+  let bottomDescription = document.createElement("div");
+  bottomDescription.classList.add("bottom-description");
+  simpleUser.appendChild(bottomDescription);
+
+  let location = document.createElement("h4");
+  location.textContent = user.location;
+  bottomDescription.appendChild(location);
+
+  let followers = document.createElement("h4");
+  followers.textContent = `Followers: ${user.followers}`;
+  bottomDescription.appendChild(followers);
+
+  return simpleUser;
+}
