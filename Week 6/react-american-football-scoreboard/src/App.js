7dae2beff48278d250f6f2109cb61127640554fd
Kate Efimova <kate.efimova0@gmail.com>
2019-08-12 09:21:12 -0700

    Broke down a component into several components

--- Week 6/react-american-football-scoreboard/src/App.js
+++ Week 6/react-american-football-scoreboard/src/App.js
@@ -4,27 +4,32 @@ import { render } from "react-dom";
 import BottomRow from "./BottomRow";
 import { useState } from "react";
 
+function scoreboard(homeScore, awayScore) {
+  return (
+    <section className="scoreboard">
+      <div className="topRow">
+        <div className="home">
+          <h2 className="home__name">Lions</h2>
+
+          <div className="home__score">{homeScore}</div>
+        </div>
+        <div className="timer">00:03</div>
+        <div className="away">
+          <h2 className="away__name">Tigers</h2>
+          <div className="away__score">{awayScore}</div>
+        </div>
+      </div>
+      <BottomRow />
+    </section>
+  );
+}
+
 function App() {
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
-
   return (
     <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            <div className="home__score">{homeScore}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
+      {scoreboard(homeScore, awayScore)}
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
