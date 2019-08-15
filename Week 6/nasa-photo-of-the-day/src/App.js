aa64bf4ce2b8a2f7f84ecd73e19da4758f60069c
Kate Efimova <kate.efimova0@gmail.com>
2019-08-14 18:21:27 -0700

    Gets data w/ axios

--- Week 6/nasa-photo-of-the-day/src/App.js
+++ Week 6/nasa-photo-of-the-day/src/App.js
@@ -1,14 +1,30 @@
 import React from "react";
 import "./App.css";
+import { useState, useEffect } from "react";
+const axios = require("axios");
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+        .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+        .then(res => {
+          if (!res) setData("hi");
+          else setData(res.data);
+        })
+        .catch(err => console.log("Error message", err));
+    };
+
+    console.log(data);
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
+    <>
+      <p>I'm a paragraph tag</p>
+    </>
   );
 }
 
