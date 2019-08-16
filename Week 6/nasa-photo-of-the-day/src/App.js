3694b323dc611f23412269a434392b8b1831d4d0
Kate Efimova <kate.efimova0@gmail.com>
2019-08-15 18:11:05 -0700

    :fire:

--- Week 6/nasa-photo-of-the-day/src/App.js
+++ Week 6/nasa-photo-of-the-day/src/App.js
@@ -1,30 +1,65 @@
 import React from "react";
 import "./App.css";
+import styled from "styled-components";
 import { useState, useEffect } from "react";
 const axios = require("axios");
 
+const Img = styled.img`
+  width: 100%;
+  height: auto;
+  border: 3px solid white;
+`;
+
+const Container = styled.div`
+  height: auto;
+  width: 80vh;
+  padding: 5em;
+`;
+
+const CoolBackground = styled.div`
+  height: auto;
+  width: 100%;
+  background-image: linear-gradient(
+    to right top,
+    #845ec2,
+    #d65db1,
+    #ff6f91,
+    #ff9671,
+    #ffc75f,
+    #f9f871
+  );
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState("Data");
 
   useEffect(() => {
     const fetchData = () => {
       axios
         .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
         .then(res => {
-          if (!res) setData("hi");
-          else setData(res.data);
+          console.log(res.data);
+          setData(res.data);
         })
         .catch(err => console.log("Error message", err));
     };
 
-    console.log(data);
     fetchData();
   }, []);
 
   return (
-    <>
-      <p>I'm a paragraph tag</p>
-    </>
+    <CoolBackground>
+      <Container>
+        <Img src={data.url} />
+        <h1>{data.title}</h1>
+        <p>{data.explanation}</p>
+        <h4>Image taken on {data.date}</h4>
+      </Container>
+    </CoolBackground>
   );
 }
 
