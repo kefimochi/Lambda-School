0e27d61d6050c29446e979ed9866cabe0d565c3b
Kate Efimova <kate.efimova0@gmail.com>
2019-08-22 13:20:26 -0700

    MVP reached

--- Week 7/team-builder/src/App.js
+++ Week 7/team-builder/src/App.js
@@ -6,13 +6,16 @@ import Form from "./Form";
 
 function App() {
   const [usersArray, setUsersArray] = useState([]);
+  const memberToEdit = props => {
+    console.log("props", props);
+  };
   return (
-    <>
+    <div class="container-all">
       <Form usersArray={usersArray} setUsersArray={setUsersArray} />
       {usersArray.map(user => (
-        <Card user={user} />
+        <Card user={user} memberToEdit={memberToEdit} />
       ))}
-    </>
+    </div>
   );
 }
 
