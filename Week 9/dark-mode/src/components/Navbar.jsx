d4e0c9177b5042a34c39251c379675cc63f7e549
Kate Efimova <kate.efimova0@gmail.com>
2019-09-04 20:46:00 -0700

    Finished :fire:

--- Week 9/dark-mode/src/components/Navbar.jsx
+++ Week 9/dark-mode/src/components/Navbar.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import useDarkMode from "../hooks/useDarkMode";
+import "../styles.scss";
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
   };
+  console.log("Dakr mode", darkMode);
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
       <div className="dark-mode__toggle">
         <div
           onClick={toggleMode}
-          className={darkMode ? 'toggle toggled' : 'toggle'}
+          className={!darkMode ? "dark-mode toggle" : "toggle toggled"}
         />
       </div>
     </nav>
