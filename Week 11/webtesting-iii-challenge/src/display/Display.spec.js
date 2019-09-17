60f240389e762bba88ea590ae482bddb0a4c7601
Kate Efimova <kate.efimova0@gmail.com>
2019-09-16 23:23:31 -0700

    Testing

--- Week 11/webtesting-iii-challenge/src/display/Display.spec.js
+++ Week 11/webtesting-iii-challenge/src/display/Display.spec.js
@@ -2,28 +2,15 @@ import React from "react";
 import * as rtl from "react-testing-library";
 import Display from "./Display";
 
-test("Testing if closed, should be red", () => {
-  const { locked } = rtl.render(<Display closed={true} locked={true} />);
-  console.log("locked", locked);
-  //   expect(locked.classList).toContain("red-led");
+test("Testing if closed OR locked, should be red", () => {
+  const { getByText } = rtl.render(<Display closed={true} locked={true} />);
+  const closed = getByText(/closed/i);
+  const locked = getByText(/locked/i);
+  expect(locked.classList).toContain("red-led");
+  expect(closed.classList).toContain("red-led");
 });
 
-//   test("Testing if locked, should be red", () => {
-//     const { getByText } = rtl.render(<Display closed={false} locked={false} />);
-//   });
-
-// test("Testing for display of open and unlocked gate", () => {
-//   const { getByText } = rtl.render(<Display closed={false} locked={false} />);
-// });
-
-// test("Testing for display of open/closed and locked/unlocked", () => {
-//     const {getByText} = render(<Select />)
-// });
-
-// test("Testing for display of open/closed and locked/unlocked", () => {
-//     const {getByText} = render(<Select />)
-// });
-
-// test("Testing for display of open/closed and locked/unlocked", () => {
-//     const {getByText} = render(<Select />)
-// });
+test("matches snapshot", () => {
+  const { container } = rtl.render(<Display />);
+  expect(container).toMatchSnapshot();
+});
