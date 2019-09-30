1f4e843356e4c73b8d67111e8b49938c9d2694a7
Kate Efimova <kate.efimova0@gmail.com>
2019-09-30 11:40:41 -0700

    Getting started on server responses

--- Week 13/node-api1-project/index.js
+++ Week 13/node-api1-project/index.js
@@ -1,1 +1,22 @@
 // implement your API here
+const express = require("express");
+const port = 5000;
+const server = express();
+
+server.get("/hobbits", (req, res) => {
+  res.send("Welcome to Hobbiton");
+});
+
+server.post("/hobbits", (req, res) => {
+  res.status(201).json({ url: "/hobbits", operation: "POST" });
+});
+
+server.put("/hobbits", (req, res) => {
+  res.status(201).json({ url: "/hobbits", operation: "PUT" });
+});
+
+server.delete("/hobbits", (req, res) => {
+  res.sendStatus(204);
+});
+
+server.listen(port, () => {});
