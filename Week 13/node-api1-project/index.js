afcc198c54d059e3b422d249767b467a132def0d
Kate Efimova <kate.efimova0@gmail.com>
2019-09-30 13:04:13 -0700

    In process

--- Week 13/node-api1-project/index.js
+++ Week 13/node-api1-project/index.js
@@ -1,22 +1,36 @@
-// implement your API here
+const db = require("./data/db");
 const express = require("express");
 const port = 5000;
 const server = express();
 
-server.get("/hobbits", (req, res) => {
-  res.send("Welcome to Hobbiton");
+server.use(express.json());
+
+server.get("/api/users", (req, res) => {
+  console.log(req.body);
+  //   res.status(200).json(db.find());
+  res.status(200);
 });
 
-server.post("/hobbits", (req, res) => {
-  res.status(201).json({ url: "/hobbits", operation: "POST" });
+server.post("/api/users", (req, res) => {
+  console.log(req.body);
+  //   if (!req.body.name || !req.body.bio) {
+  //     res
+  //       .status(400)
+  //       .json({ errorMessage: "Please provide name and bio for the user." });
+  //   } else {
+  //     res.status(201).json({ url: "/hobbits", operation: "POST" });
+  //   }
+  res.status(201).send(req.body);
 });
 
 server.put("/hobbits", (req, res) => {
-  res.status(201).json({ url: "/hobbits", operation: "PUT" });
+  res.status(200).json({ url: "/hobbits", operation: "PUT" });
 });
 
 server.delete("/hobbits", (req, res) => {
   res.sendStatus(204);
 });
 
-server.listen(port, () => {});
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
