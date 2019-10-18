c408839799b9f72938b94d8a4af7ccaf75542f71
Kate Efimova <kate.efimova0@gmail.com>
2019-10-18 09:14:18 -0700

    Tested logging in

--- Week 15/Sprint-Challenge-Authentication/auth/auth.spec.js
+++ Week 15/Sprint-Challenge-Authentication/auth/auth.spec.js
@@ -1,3 +1,37 @@
 const auth = require("./auth-router");
-const auth = require("./authenticate-middleware");
+const authMiddleware = require("./authenticate-middleware");
 const db = require("../database/dbConfig");
+const supertest = require("supertest");
+const server = require("../api/server");
+
+describe("POST / login", () => {
+  it("should return status 200 if the user successfully logged on", async () => {
+    let response = await supertest(server)
+      .post("/api/auth/login")
+      .send({
+        username: "admin",
+        password: "password"
+      });
+    expect(response.status).toBe(200);
+  });
+  it("should return a generated token in case of successful login", async () => {
+    let response = await supertest(server)
+      .post("/api/auth/login")
+      .send({
+        username: "admin",
+        password: "password"
+      });
+    expect(response.body.token).toBeTruthy();
+  });
+  it("should return 401 unauthorized if the user doesn't exist on the database", async () => {
+    let response = await supertest(server)
+      .post("/api/auth/login")
+      .send({
+        username: "admin",
+        password: "123"
+      });
+    expect(response.status).toBe(401);
+  });
+});
+
+describe("POST / register", () => {});
--- Week 15/Sprint-Challenge-Authentication/jokes/jokes.spec.js
+++ Week 15/Sprint-Challenge-Authentication/jokes/jokes.spec.js
@@ -1,2 +1,11 @@
 const Hobbits = require("./jokes-router");
 const db = require("../database/dbConfig");
+const supertest = require("supertest");
+const server = require("../api/server");
+
+describe("GET / jokes model", () => {
+  it("should return 401 hhtp status code when no user is logged in", async () => {
+    let response = await supertest(server).get("/api/jokes");
+    expect(response.status).toBe(401);
+  });
+});
